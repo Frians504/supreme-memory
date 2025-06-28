c72d38776c7cdd91185f98496d05721979f1fe48
Frians504 <cyberjatim482@gmail.com>
2025-06-28 13:47:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-
+#!/bin/bash
+clear
+echo -e "\e[92mInstalling dependencies...\e[0m"
+pkg install python -y
+pip install -r requirements.txt
+echo -e "\e[92mDone. Jalankan dengan:\e[0m python tracker.py"
